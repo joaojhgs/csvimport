6b6509d8a27d026750ba976ae03d61c513166878
joaohgs <joaosperandio@alunos.utfpr.edu.br>
2023-08-27 16:30:09 -0300

    feat(frontend): initial search and cards display setup

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { ConfigProvider } from 'antd';
+import React, { useState } from 'react';
+import { ConfigProvider, Layout, theme, Input, Card } from 'antd'
+const { Search } = Input;
+const { Content, Footer } = Layout;
+const initialCards = [
+  { title: 'Card 1', content: 'Content for Card 1' },
+  { title: 'Card 2', content: 'Content for Card 2' },
+];
+const App: React.FC = () => {
+  const [searchText, setSearchText] = useState('');
+  const [filteredCards, setFilteredCards] = useState(initialCards);
 
-function App() {
+  const handleSearch = (value: string) => {
+    const filtered = initialCards.filter(card =>
+      card.title.toLowerCase().includes(value.toLowerCase())
+    );
+    setSearchText(value);
+    setFilteredCards(filtered);
+  };
   return (
-    <div className="App">
-      <ConfigProvider theme={{ token: { colorPrimary: '#00b96b' } }}>
-      </ConfigProvider>
-    </div>
+    <ConfigProvider theme={{ algorithm: theme.darkAlgorithm, token: { colorPrimary: '#00b96b' } }}>
+      <Layout className="layout" style={{ width: '100vw', height: '100vh' }}>
+        <Content style={{ padding: '0 50px', width: '100%', height: '100%' }}>
+          <div style={{ padding: '20px' }}>
+            <Search
+              placeholder="Search cards..."
+              allowClear
+              enterButton="Search"
+              size="large"
+              value={searchText}
+              onChange={e => setSearchText(e.target.value)}
+              onSearch={handleSearch}
+            />
+            <div style={{ marginTop: '20px' }}>
+              {filteredCards.map((card, index) => (
+                <Card key={index} title={card.title}>
+                  {card.content}
+                </Card>
+              ))}
+            </div>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Joaojhgs ©2023</Footer>
+      </Layout>
+    </ConfigProvider>
   );
-}
+};
 
 export default App;
